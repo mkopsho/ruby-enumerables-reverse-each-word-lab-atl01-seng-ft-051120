4ea528f9b38d03fd6ac92e9a5970b7a57e74ca5d
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-17 15:51:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,7 +9,7 @@ def reverse_each_word(string)
   # With `collect`!
   string_array = string.split(" ")
   string_array.collect do |word|
-    word.reverse
+    word.reverse!
   end
   string_array.join(" ")
 end
